d4307cb93d63bdf53a36ee6efeac0ebda4302074
Brandon <bigoud33@gmail.com>
2019-05-10 16:50:58 +0200

    remove certificate

--- app/src/main/java/com/example/studentcheck/LoginActivity.kt
+++ app/src/main/java/com/example/studentcheck/LoginActivity.kt
@@ -91,51 +91,10 @@ class LoginActivity : AppCompatActivity() {
 
     fun provideHttpClientBuilder(loggingInterceptor: HttpLoggingInterceptor, context: Context): OkHttpClient.Builder {
 
-        // Load CAs from an InputStream
-        val certificateFactory = CertificateFactory.getInstance("X.509")
-
-        // Load self-signed certificate (*.crt file)
-        val inputStream =  context.resources.openRawResource(R.raw.bigoudinc)
-        val certificate = certificateFactory.generateCertificate(inputStream)
-        inputStream.close()
-
-        // Create a KeyStore containing our trusted CAs
-        val keyStoreType = KeyStore.getDefaultType()
-        val keyStore = KeyStore.getInstance(keyStoreType)
-        keyStore.load(null, null)
-        keyStore.setCertificateEntry("ca", certificate)
-
-        // Create a TrustManager that trusts the CAs in our KeyStore.
-        val tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm()
-        val trustManagerFactory = TrustManagerFactory.getInstance(tmfAlgorithm)
-        trustManagerFactory.init(keyStore)
-
-        val trustManagers = trustManagerFactory.trustManagers
-        val x509TrustManager = trustManagers[0] as X509TrustManager
-
-        // Create an SSLSocketFactory that uses our TrustManager
-        val sslContext = SSLContext.getInstance("SSL")
-        sslContext.init(null, arrayOf(x509TrustManager), null)
-        var sslSocketFactory = sslContext.socketFactory
-
-
-
         return OkHttpClient.Builder()
             .readTimeout(15, TimeUnit.SECONDS)
             .writeTimeout(15, TimeUnit.SECONDS)
             .addNetworkInterceptor(loggingInterceptor)
-            .sslSocketFactory(sslSocketFactory, x509TrustManager)
-            .hostnameVerifier(myHostNameVerifier())
-    }
-
-    private fun myHostNameVerifier(): HostnameVerifier {
-        return HostnameVerifier { hostname, _ ->
-            if (hostname == "bigoud.games") {
-                return@HostnameVerifier true
-            }
-
-            false
-        }
     }
 
     fun provideGson(): Gson =
