9e27092619dc8e234638b489222fc27edce1f32d
Brandon <bigoud33@gmail.com>
2019-05-10 13:23:37 +0200

    remove certificate

--- app/src/main/java/com/example/studentcheck/MainActivity.kt
+++ app/src/main/java/com/example/studentcheck/MainActivity.kt
@@ -1,9 +1,8 @@
 package com.example.studentcheck
 
-import android.content.Context
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.support.v7.app.AppCompatActivity
 import android.view.WindowManager
 import com.github.ajalt.timberkt.Timber
 import com.google.gson.Gson
@@ -18,13 +17,7 @@ import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import java.security.KeyStore
-import java.security.cert.CertificateFactory
 import java.util.concurrent.TimeUnit
-import javax.net.ssl.HostnameVerifier
-import javax.net.ssl.SSLContext
-import javax.net.ssl.TrustManagerFactory
-import javax.net.ssl.X509TrustManager
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,7 +30,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        clientBuilder = provideHttpClientBuilder(provideHttpLoggingInterceptor(), this)
+        clientBuilder = provideHttpClientBuilder(provideHttpLoggingInterceptor())
         retrofit = provideRetrofit(provideGson(), clientBuilder.build())
 
         val strBarcode = intent.getStringExtra("barcode")
@@ -77,53 +70,12 @@ class MainActivity : AppCompatActivity() {
             level = HttpLoggingInterceptor.Level.BODY
         }
 
-    fun provideHttpClientBuilder(loggingInterceptor: HttpLoggingInterceptor, context: Context): OkHttpClient.Builder {
-
-        // Load CAs from an InputStream
-        val certificateFactory = CertificateFactory.getInstance("X.509")
-
-        // Load self-signed certificate (*.crt file)
-        val inputStream =  context.resources.openRawResource(R.raw.bigoudinc)
-        val certificate = certificateFactory.generateCertificate(inputStream)
-        inputStream.close()
-
-        // Create a KeyStore containing our trusted CAs
-        val keyStoreType = KeyStore.getDefaultType()
-        val keyStore = KeyStore.getInstance(keyStoreType)
-        keyStore.load(null, null)
-        keyStore.setCertificateEntry("ca", certificate)
-
-        // Create a TrustManager that trusts the CAs in our KeyStore.
-        val tmfAlgorithm = TrustManagerFactory.getDefaultAlgorithm()
-        val trustManagerFactory = TrustManagerFactory.getInstance(tmfAlgorithm)
-        trustManagerFactory.init(keyStore)
-
-        val trustManagers = trustManagerFactory.trustManagers
-        val x509TrustManager = trustManagers[0] as X509TrustManager
-
-        // Create an SSLSocketFactory that uses our TrustManager
-        val sslContext = SSLContext.getInstance("SSL")
-        sslContext.init(null, arrayOf(x509TrustManager), null)
-        var sslSocketFactory = sslContext.socketFactory
-
-
+    fun provideHttpClientBuilder(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient.Builder {
 
         return OkHttpClient.Builder()
             .readTimeout(15, TimeUnit.SECONDS)
             .writeTimeout(15, TimeUnit.SECONDS)
             .addNetworkInterceptor(loggingInterceptor)
-            .sslSocketFactory(sslSocketFactory, x509TrustManager)
-            .hostnameVerifier(myHostNameVerifier())
-    }
-
-    private fun myHostNameVerifier(): HostnameVerifier {
-        return HostnameVerifier { hostname, _ ->
-            if (hostname == "bigoud.games") {
-                return@HostnameVerifier true
-            }
-
-            false
-        }
     }
 
     fun provideGson(): Gson =
